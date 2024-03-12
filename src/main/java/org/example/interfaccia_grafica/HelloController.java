15c7013427b7c52fc4401e09b63d6e0987ff9142
alex <aless.distef@gmail.com>
2024-03-12 17:45:29 +0100

    update

--- src/main/java/org/example/interfaccia_grafica/HelloController.java
+++ src/main/java/org/example/interfaccia_grafica/HelloController.java
@@ -1,10 +1,7 @@
 package org.example.interfaccia_grafica;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -20,6 +17,9 @@ public class HelloController {
     private TextField admin_cognome;
 
     @FXML
+    private AnchorPane admin_form;
+
+    @FXML
     private Button admin_login;
 
     @FXML
@@ -32,10 +32,28 @@ public class HelloController {
     private PasswordField admin_password;
 
     @FXML
+    private Hyperlink hyperlink_admin;
+
+    @FXML
+    private Hyperlink hyperlink_utente;
+
+    @FXML
+    private Button login_accedi_admin;
+
+    @FXML
+    private Button login_accedi_utente;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
     private Button utente_accedi;
 
     @FXML
     private ImageView utente_close;
+
+    @FXML
+    private ImageView utente_close1;
 
     @FXML
     private TextField utente_cognome;
@@ -44,13 +62,16 @@ public class HelloController {
     private TextField utente_eta;
 
     @FXML
+    private AnchorPane utente_form;
+
+    @FXML
     private ImageView utente_minimize;
 
     @FXML
-    private TextField utente_nome;
+    private ImageView utente_minimize1;
 
     @FXML
-    private AnchorPane utente_form;
+    private TextField utente_nome;
 
     public void utente_close(){
         System.exit(0);
@@ -60,6 +81,44 @@ public class HelloController {
         Stage stage = (Stage)utente_form.getScene().getWindow();
         stage.setIconified(true);
     }
+
+    public void admin_close(){
+        System.exit(0);
+    }
+
+    public void admin_minimize(){
+        Stage stage = (Stage)admin_form.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    // Metodo per mostrare il pannello dell'utente
+
+    @FXML
+    private void handleLoginUtente() {
+        utente_form.setVisible(true);
+        admin_form.setVisible(false);
+    }
+
+    // Metodo per mostrare il pannello dell'admin
+    @FXML
+    private void handleLoginAdmin() {
+        admin_form.setVisible(true);
+        utente_form.setVisible(false);
+    }
+
+    @FXML
+    private void handleBackToMainFromAdmin() {
+        admin_form.setVisible(false); // Nasconde il form admin
+        login_form.setVisible(true); // Mostra il form principale
+    }
+
+    @FXML
+    private void handleBackToMainFromUtente() {
+        utente_form.setVisible(false); // Nasconde il form utente
+        login_form.setVisible(true); // Mostra il form principale
+    }
+
+
 
     @FXML
     protected void onHelloButtonClick() {
